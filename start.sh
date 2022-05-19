7dcb5a9f06e125e1de991f976e5e8e0d23c05bd8
Nimrod Chitayi <nimrod.chitayi@gmail.com>
2022-05-19 09:52:38 +0300

    exported database url and email server

--- start.sh
+++ start.sh
@@ -1,1 +1,7 @@
+export SECRET_KEY='dbb0a75595cd8924bd0ec31c43898697'
+export SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://nimrod:*admin*@localhost:5432/one_mic'
+export MAIL_USERNAME='micmojainc@gmail.com'
+export MAIL_PASSWORD='HakunaPassword'
+
+
 python3 manage.py server
