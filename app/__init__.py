e828652de71ccce56cf81f417504af8f50b10a6e
Nimrod Chitayi <nimrod.chitayi@gmail.com>
2022-05-19 10:12:12 +0300

    updated init file

--- app/__init__.py
+++ app/__init__.py
@@ -1,36 +1,36 @@
+import os
 from flask import Flask
-from config import configurations
 from flask_sqlalchemy import SQLAlchemy
-# from flask_bootstrap import Bootstrap
+from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-from flask_uploads import configure_uploads,IMAGES,UploadSet
+from config import config_options
+# from flask_mail import Mail
+
+
+
 
 app = Flask(__name__)
 db = SQLAlchemy()
-# bootstrap = Bootstrap()
+bcrypt = Bcrypt()
 login_manager = LoginManager()
-login_manager.session_protection = 'strong'
-login_manager.login_view = 'auth.login'
-photos = UploadSet('photos',IMAGES)
+login_manager.login_view = 'users.login'
+login_manager.login_message_category = 'info'
 
-def create_app(configname):
-    '''
-    Method for creating the app to enable for easier configurations
-    '''
-    app.config.from_object(configurations[configname])
 
-    #Importing BluePrints
-    from .auth import auth as auth_blueprint
-    from .main import main as main_blueprint
-    app.register_blueprint(auth_blueprint)
-    app.register_blueprint(main_blueprint)
+def create_app(config_name):
+      app.config.from_object(config_options[config_name])
+      db.init_app(app)
+      login_manager.init_app(app)
+      bcrypt.init_app(app)
 
-    #Initialising the Importations
-    db.init_app(app)
-#     bootstrap.init_app(app)
-    login_manager.init_app(app)
 
-    # configure UploadSet
-    configure_uploads(app,photos)
+      from app.users.views import users
+      from app.pitches.views import pitches
+      from app.main.views import main
 
-    return app
+      app.register_blueprint(users)
+      app.register_blueprint(pitches)
+      app.register_blueprint(main)
+
+
+      return app
